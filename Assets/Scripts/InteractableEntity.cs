68a6047308b9fbae051ada786dbd1311327e736f
Ben Rokah <benk0914@gmail.com>
2019-06-19 23:41:18 +0300

    Interactables & repositioning

---
+++ Assets/Scripts/InteractableEntity.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InteractableEntity : MonoBehaviour
+{
+    [SerializeField]
+    public string Name;
+
+    [SerializeField]
+    public UnityEvent Actions = new UnityEvent();
+
+    public void Interact()
+    {
+        Actions.Invoke();
+    }
+}
---
+++ Assets/Scripts/InteractionRay.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionRay : MonoBehaviour
+{
+    RaycastHit rhit;
+
+    Collider CurrentCollider = null;
+
+    [SerializeField]
+    LayerMask HitMask;
+
+    InteractableEntity CurrentInteractable = null;
+
+    private void Update()
+    {
+        Physics.Raycast(transform.position, transform.forward,  out rhit, HitMask);
+
+        if (rhit.collider != null)
+        {
+            if (rhit.collider != CurrentCollider)
+            {
+                CurrentInteractable = rhit.collider.GetComponent<InteractableEntity>();
+                if (CurrentInteractable != null)
+                {
+                    OnHover();
+                    return;
+                }
+            }
+        }
+
+        CurrentInteractable = null;
+        UnHover();
+    }
+
+    private void LateUpdate()
+    {
+        if(Input.GetKey(InputMap.Map["Interact"]))
+        {
+            Interact();
+        }
+    }
+
+    void Interact()
+    {
+        if(CurrentInteractable == null)
+        {
+            return;
+        }
+
+        CurrentInteractable.Interact();
+    }
+
+    void OnHover()
+    {
+        CursorTooltipUI.Instance.Show(CurrentInteractable.Name);
+    }
+
+    void UnHover()
+    {
+        CursorTooltipUI.Instance.Hide();
+    }
+}
