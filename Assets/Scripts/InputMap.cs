4adb7da67eea1b0da75a79dec0af4cc352fe72ca
Ben Rokah <benk0914@gmail.com>
2019-06-19 19:50:09 +0300

    ThE sPIDER First Commit

---
+++ Assets/Scripts/InputMap.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+public class InputMap : MonoBehaviour {
+
+    [SerializeField]
+    List<InputKeyPair> InitList = new List<InputKeyPair>();
+     
+    public static Dictionary<string, KeyCode> Map = new Dictionary<string, KeyCode>();
+
+    private void Awake()
+    {
+        Initialize();
+    }
+
+    public void Initialize()
+    {
+        Map.Clear();
+
+        for (int i = 0; i < InitList.Count; i++)
+        {
+            Map.Add(InitList[i].name, InitList[i].KeyCode);
+        }
+
+        LoadMap();
+    }
+
+    public static void LoadMap()
+    {
+        string currentKey;
+        int currentValue;
+
+        int i = 0;
+        while (PlayerPrefs.HasKey("inputMapKey_" + i))
+        {
+            currentKey = PlayerPrefs.GetString("inputMapKey_" + i);
+            currentValue = PlayerPrefs.GetInt("inputMapValue_" + i);
+
+            if (Map.ContainsKey(currentKey))
+            {
+                Map[currentKey] = (KeyCode)currentValue;
+            }
+            else
+            {
+                Map.Add(currentKey, (KeyCode)currentValue);
+            }
+
+            i++;
+        }
+    }
+
+    public static void SaveMap()
+    {
+        for (int i = 0; i < Map.Keys.Count; i++)
+        {
+            PlayerPrefs.SetString("inputMapKey_" + i, Map.Keys.ElementAt(i));
+            PlayerPrefs.SetInt("inputMapValue_" + i, (int) Map[Map.Keys.ElementAt(i)]);
+        }
+
+        PlayerPrefs.Save();
+    }
+
+}
---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("Camera-Control/Mouse Look")]
+public class MouseLook : MonoBehaviour
+{
+
+    public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
+    public RotationAxes axes = RotationAxes.MouseXAndY;
+    public float sensitivityX = 15F;
+    public float sensitivityY = 15F;
+
+    public float minimumX = -360F;
+    public float maximumX = 360F;
+
+    public float minimumY = -60F;
+    public float maximumY = 60F;
+
+    public float MovementSpeed = 10.0f;
+
+    float rotationY = 0F;
+
+    [SerializeField]
+    Transform CameraTransform;
+
+    private float translation;
+    private float straffe;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        //Movement
+        if (Input.GetKey(InputMap.Map["MoveForward"]))
+        {
+            transform.position += transform.forward * MovementSpeed * Time.deltaTime;
+        }
+        else if (Input.GetKey(InputMap.Map["MoveBackward"]))
+        {
+            transform.position += -transform.forward * MovementSpeed / 2f * Time.deltaTime;
+        }
+
+        if (Input.GetKey(InputMap.Map["MoveLeft"]))
+        {
+            transform.position += -transform.right * MovementSpeed / 2f * Time.deltaTime;
+        }
+        else if (Input.GetKey(InputMap.Map["MoveRight"]))
+        {
+            transform.position += transform.right * MovementSpeed / 2f * Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+
+        //Look Around
+        if (axes == RotationAxes.MouseXAndY)
+        {
+            float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+            transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+            CameraTransform.localEulerAngles = new Vector3(-rotationY, CameraTransform.localEulerAngles.y, 0);
+
+        }
+        else if (axes == RotationAxes.MouseX)
+        {
+            transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+        }
+        else
+        {
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+            CameraTransform.localEulerAngles = new Vector3(-rotationY, CameraTransform.localEulerAngles.y, 0);
+        }
+    }
+
+}
