db87afd4be9b594799b4d48dd3c12be9f60ba339
Ben Rokah <benk0914@gmail.com>
2019-06-24 22:58:25 +0300

    Fixes and changes

---
+++ Assets/Scripts/Misc/FaceCameraScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceCameraScript : MonoBehaviour
+{
+    [SerializeField]
+    Camera m_Camera;
+
+    private void Update()
+    {
+       transform.LookAt(transform.position + m_Camera.transform.rotation * Vector3.forward,
+            m_Camera.transform.rotation * Vector3.up);
+    }
+}
