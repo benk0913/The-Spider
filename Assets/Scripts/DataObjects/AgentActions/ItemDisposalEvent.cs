63dbd30ea05d183474fe2078bc944c6755cf1268
Ben Rokah <benk0914@gmail.com>
2020-02-14 17:50:30 +0200

    More oracles quest / miscs

---
+++ Assets/Scripts/DataObjects/AgentActions/ItemDisposalEvent.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "ItemDisposalEvent", menuName = "DataObjects/AgentActions/Item/ItemDisposalEvent", order = 2)]
+public class ItemDisposalEvent : AgentAction
+{
+    public string InvokeEventKey;
+
+    public List<Item> OtherRequiredItems = new List<Item>();
+
+    public List<Item> CanBeOnlyDoneWithItems = new List<Item>();
+
+    public override void Execute(Character requester, Character character, AgentInteractable target)
+    {
+        base.Execute(requester, character, target);
+
+        FailReason reason;
+        if (!CanDoAction(requester, character, target, out reason))
+        {
+            return;
+        }
+
+        ItemUI itemUI = (ItemUI)target;
+
+        character.TopEmployer.Belogings.Remove(itemUI.CurrentItem);
+        InventoryPanelUI.Instance.ItemWasAdded(0);
+
+        CORE.Instance.InvokeEvent(InvokeEventKey);
+    }
+
+    public override bool CanDoAction(Character requester, Character character, AgentInteractable target, out FailReason reason)
+    {
+        ItemUI item = (ItemUI)target;
+
+        reason = null;
+
+        if(CanBeOnlyDoneWithItems.Find(x=>x.name == item.CurrentItem.name) == null)
+        {
+            return false;
+        }
+
+        if (!base.CanDoAction(requester, character, target, out reason))
+        {
+            return false;
+        }
+
+        foreach(Item reqItem in OtherRequiredItems)
+        {
+            if(character.TopEmployer.Belogings.Find(x=>x.name == reqItem.name) == null)
+            {
+                reason = new FailReason("Requires Item - " + reqItem.name);
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ Assets/Scripts/DataObjects/Quests/Rewards/QRGainLetter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "QRGainLetter", menuName = "DataObjects/Quests/Rewards/QRGainLetterSe", order = 2)]
+public class QRGainLetter : QuestReward
+{
+    [SerializeField]
+    public LetterPreset Letter;
+
+    public override void Claim(Character byCharacter)
+    {
+        base.Claim(byCharacter);
+
+        Dictionary<string, object> parameters = new Dictionary<string, object>();
+        parameters.Add("Letter_From", this.Letter.PresetSender);
+        parameters.Add("Letter_To", CORE.PC);
+
+        Letter letter = new Letter(this.Letter, parameters);
+
+        LetterDispenserEntity.Instance.DispenseLetter(letter);
+    }
+}
