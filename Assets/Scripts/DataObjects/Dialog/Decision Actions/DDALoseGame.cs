a04dadee0fca917b32e8037657fbcdafac197111
Ben Rokah <benk0914@gmail.com>
2020-02-29 13:29:57 +0200

    Main Quest - Main Path - Complete

---
+++ Assets/Scripts/DataObjects/Dialog/Decision Actions/DDALoseGame.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "DDALoseGame", menuName = "DataObjects/Dialog/Actions/DDALoseGame", order = 2)]
+public class DDALoseGame : DialogDecisionAction
+{
+
+    public override void Activate()
+    {
+        LoseWindowUI.Instance.Show();
+    }
+}
