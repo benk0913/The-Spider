62611f527e08c9abfe62276d8e244f4f79fc13e7
Ben Rokah <benk0914@gmail.com>
2020-03-02 13:54:24 +0200

    Fayettes Path Progress

---
+++ Assets/Scripts/DataObjects/Dialog/Decision Actions/DDATakeOverFaction.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "DDATakeOverFaction", menuName = "DataObjects/Dialog/Actions/DDATakeOverFaction", order = 2)]
+public class DDATakeOverFaction : DialogDecisionAction
+{
+    [SerializeField]
+    Faction faction;
+
+    public override void Activate()
+    {
+        Character factionHead = CORE.Instance.Characters.Find(x => x.CurrentFaction.name == faction.name && x.TopEmployer == x);
+
+        if(factionHead == null)
+        {
+            Debug.LogError("Couldn't find faction head for faction: " + faction.name);
+            return;
+        }
+
+        factionHead.StartWorkingFor(CORE.PC.PropertiesOwned[0]);
+        factionHead.AI = null;
+    }
+}
