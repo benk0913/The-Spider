c894a09120899e09e0d6d4fca49d7cb8e8da942c
benk0913 <benk0913@gmail.com>
2021-01-13 15:43:23 +0200

    Pre textmeshpro

---
+++ Assets/Scripts/DataObjects/Quests/Objectives/QOHasEmployeesCount.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "QOHasEmployeesCount", menuName = "DataObjects/Quests/QuestObjectives/QOHasEmployeesCount", order = 2)]
+public class QOHasEmployeesCount : QuestObjective
+{
+    bool valid = false;
+    public int count = 1;
+
+    int currentCount = 0;
+
+    public override bool Validate()
+    {
+        return CORE.PC.CharactersInCommand.Count > count;
+    }
+
+    void OnEventInvoked()
+    {
+        valid = true;
+    }
+}
